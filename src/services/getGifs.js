78e81fb091d5e13ef27c8a0eb0a5bb99b5f700ad
Dtheran <dtheran1@gmail.com>
2022-10-07 08:16:45 -0500

    init

---
+++ src/services/getGifs.js
@@ -0,0 +1,13 @@
+const apiKey = '19B5B1KbY7NstETrqnE05DunzVRVlV9G&q'
+
+export default function getGifs({ keyword = 'morty' } = {}) {
+    const apiURL = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}=${keyword}&limit=10&offset=0&rating=pg-13&lang=es`
+
+    return fetch(apiURL)
+        .then((res) => res.json())
+        .then(response => {
+            const { data } = response
+            const gifs = data.map(image => image.images.downsized_medium.url)
+            return gifs
+        })
+}
