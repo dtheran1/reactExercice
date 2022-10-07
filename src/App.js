161b35b8011bcd461d94922b22ddb662845d78d6
Dtheran <dtheran1@gmail.com>
2022-10-07 10:32:46 -0500

    componetizando la app

--- src/App.js
+++ src/App.js
@@ -1,28 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import getGifs from './services/getGifs';
-import Gif from './components/Gif';
+import ListOfGifs from './components/ListOfGifs';
 
 function App() {
-  const [gifs, setGifs] = useState([]);
-
-  useEffect(function () {
-    getGifs({ keyword: 'programming' })
-      .then(gifs => setGifs(gifs))
-  }, [])
+  const [keyword, setKeyWord] = useState('cr7')
   return (
     <div className="App">
       <section className="App-content">
-        {
-          gifs.map(({ title, id, url }) =>
-        <Gif
-          key={id}
-          id={id}
-          title={title}
-          url={url}
-        />)
-        }
-        <button>Cambiar Gifs</button>
+        <button onClick={() => setKeyWord('messi')}>Cambiar keyword</button>
+        <ListOfGifs keyword={keyword} />
       </section>
     </div>
   );
